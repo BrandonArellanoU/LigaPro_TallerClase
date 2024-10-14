8df2a35e34181efbaa90de988f57e4ffb05700f2
Brandon Arellano <arebrandon12@gmail.com>
2024-10-14 08:44:16 -0500

    Creation for Jugador in Models

---
+++ LigaPro_TallerClase/Models/Estadio.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LigaPro_TallerClase.Models
+{
+    public class Estadio : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ LigaPro_TallerClase/Models/Jugador.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LigaPro_TallerClase.Models
+{
+    public class Jugador : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public int IdPlayer {  get; set; }
+
+        public int Name { get; set; }
+
+        public int position { get; set; }
+
+        public int Age { get; set; }
+
+        Equipo equipo { get; set; }
+
+
+
+    }
+}
